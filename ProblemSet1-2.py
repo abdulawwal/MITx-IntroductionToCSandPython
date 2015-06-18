1ec1698ebc55a44607c09580530a8093a260bafd
Shreyas Skandan <shreyas.ibanez@gmail.com>
2015-06-18 17:48:15 +0530

    no message

--- ProblemSet1-2.py
+++ ProblemSet1-2.py
@@ -12,3 +12,16 @@
 # s is already defined. If you are confused by this instruction, please review
 # L4 Problems 10 and 11 before you begin this problem set.
 
+s = 'azcbobobegboabghabobbobkl'
+compStr = 'bob' # The comparison string.
+index = 0 # To maintain an index system inside the looping construct. Possibly a bad technique, a result of learning code using C.
+bobCount = 0 # Number of detected substrings having the character sequence 'bob' in them
+for ch in s:
+    index += 1
+    if ch == 'b': # To stimulate the substring detection
+        tempStr = s[index-1:index+2] # Create a temporary string with the next 2 consecutive characters following the stimulant
+        # print(tempStr) if you would like to see intermediate temporary strings. 
+        if tempStr == compStr: 
+            bobCount += 1
+print('Number of bobs is: \n' + str(bobCount))     
+    
