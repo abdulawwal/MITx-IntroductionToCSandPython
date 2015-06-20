584948316bb51ca925b4ff9045392433a7cea1aa
Shreyas Skandan <shreyas.ibanez@gmail.com>
2015-06-20 21:10:54 +0530

    no message

---
+++ ProblemSet2-2.py
@@ -0,0 +1,16 @@
+# Problem Set 2 - Problem 2
+
+annualInterestRate = 0.2
+minPay = 10
+t_balance = balance = 4773 
+
+while (t_balance > 0):
+    i = 1
+    t_balance = balance
+    minPay = minPay + 10
+    while (i <= 12):
+        unpaidB = t_balance - minPay
+        interest = annualInterestRate/12.0 * unpaidB
+        t_balance = unpaidB + interest
+        i += 1
+print('Lowest payment : ' + str(minPay))
